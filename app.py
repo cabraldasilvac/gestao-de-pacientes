49eefc5647b846898e1306b99bf163863ed3552d
Wanderley Cabral da Silva <39313251+cabraldasilvac@users.noreply.github.com>
2024-05-23 14:48:15 -0300

    :rocket: feat: create database

--- app.py
+++ app.py
@@ -1,3 +1,25 @@
 import sqlite3
 
 from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+
+# Connection to database
+conn = sqlite3.connect('gestao_hospitalar.db')
+cursor = conn.cursor()
+
+#Create table if it doesn't exist
+cursor.execute(''' 
+               CREATE TABLE IF NOT EXISTS pacientes(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   nome TEXT NOT NULL,
+                   idade INTEGER,
+                   sexo TEXT,
+                   cpf TEXT UNIQUE,
+                   endereco TEXT,
+                   telefone TEXT
+               )
+            ''')
+
+conn.commit() #save
+conn.close() #close
